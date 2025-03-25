398c758ddda0006043e23d1b25a7c0ccc2b1693b
Xavics <xavics48@gmail.com>
2025-03-25 17:05:36 +0500

    Update BlogDetails.js
    billa loru

--- src/components/Blogs/BlogDetails.js
+++ src/components/Blogs/BlogDetails.js
@@ -13,7 +13,7 @@ const BlogDetails = () => {
 
   useEffect(() => {
     axios
-      .get(`${process.env.REACT_APP_API_BASE_URL}/api/blogs/${id}/`)
+      .get(`https://estate-backend-lsps.onrender.com/api/blogs/${id}/`)
       .then((response) => setBlog(response.data))
       .catch((error) => console.error("Error fetching blog data:", error));
   }, [id]);
